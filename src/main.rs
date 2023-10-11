d4288876bf9018557457cf3b5ac6110a0a16abe1
HuangWei <market@zuiyue.com>
2023-10-11 19:32:48 +0800

    创建随机文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Write;
+use rand::Rng;
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::create("r")?;
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..(10 * 1024) {
+        let mut buffer = vec![0u8; 1024];
+        rng.fill(&mut buffer[..]);
+        file.write_all(&buffer)?;
+    }
+
+    Ok(())
 }
