be795b030716f1cbb8a0a9d04bb46efc181d859b
HuangWei <market@zuiyue.com>
2023-12-30 19:13:43 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 use std::thread;
 use std::time::Duration;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cmd = "wsl";
+    let command = "wei-docker-linux redis \"https://www.zuiyue.com/discuz/clientapi.php?modac=process&modac=process&time=1703922694&uuid=a6caaa78-be7f-4447-9883-a4d5bece541f&taskUuid=484\"";
 
-fn main() {
-    println!("start");
-    test();
-    
-    println!("end");
-    thread::sleep(Duration::from_secs(10));
+    let output = match std::process::Command::new(cmd)
+        .arg("-e") // 使用 `-e` 来执行指定的命令
+        .arg("/bin/sh") // 指定使用shell
+        .arg("-c") // 告诉shell执行后面的命令字符串
+        .arg(command) // 传递整个命令字符串
+        .output() {
+            Ok(output) => output,
+            Err(e) => {
+                println!("Failed to execute process: {}", e);
+                return Ok(());
+            }
+        };
+
+    let data = format!("{}{}", 
+        std::str::from_utf8(&output.stdout)?, 
+        std::str::from_utf8(&output.stderr)?
+    );
+
+    println!("{}", data);
+
+    Ok(())
 }
 
 fn test() {
