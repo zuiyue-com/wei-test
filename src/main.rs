f5042807a64c69fe8ecb9c20bee0b5fd1ae0a614
HuangWei <market@zuiyue.com>
2024-01-16 13:34:17 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,22 @@
-use std::thread;
-use std::time::Duration;
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cmd = "wsl";
-    let command = "wei-docker-linux redis \"https://www.zuiyue.com/discuz/clientapi.php?modac=process&modac=process&time=1703922694&uuid=a6caaa78-be7f-4447-9883-a4d5bece541f&taskUuid=484\"";
+extern crate wmi;
+extern crate serde;
 
-    let output = match std::process::Command::new(cmd)
-        .arg("-e") // 使用 `-e` 来执行指定的命令
-        .arg("/bin/sh") // 指定使用shell
-        .arg("-c") // 告诉shell执行后面的命令字符串
-        .arg(command) // 传递整个命令字符串
-        .output() {
-            Ok(output) => output,
-            Err(e) => {
-                println!("Failed to execute process: {}", e);
-                return Ok(());
-            }
-        };
+use wmi::{COMLibrary, Variant, WMIConnection, WMIDateTime};
+use serde::Deserialize;
 
-    let data = format!("{}{}", 
-        std::str::from_utf8(&output.stdout)?, 
-        std::str::from_utf8(&output.stderr)?
-    );
-
-    println!("{}", data);
-
-    Ok(())
+#[derive(Deserialize)]
+struct Win32_DiskDrive {
+    Caption: String,
+    InterfaceType: String,
 }
 
-fn test() {
-    // 创建并运行第一个线程
-    thread::spawn(|| {
-        println!("Starting another thread...");
+fn main() {
+    let com_con = COMLibrary::new().unwrap();
+    let wmi_con = WMIConnection::new(com_con.into()).unwrap();
 
-        // 在第一个线程中创建并运行第二个线程
-        thread::spawn(|| {
-            thread::sleep(Duration::from_secs(5));
-            println!("Finished in the second thread");
-        });
+    let results: Vec<Win32_DiskDrive> = wmi_con.raw_query("SELECT Caption, InterfaceType FROM Win32_DiskDrive").unwrap();
 
-        println!("Finished in the first thread");
-    });
-
-    println!("exit test");
+    for drive in results {
+        println!("Caption: {}, InterfaceType: {}", drive.Caption, drive.InterfaceType);
+    }
 }
